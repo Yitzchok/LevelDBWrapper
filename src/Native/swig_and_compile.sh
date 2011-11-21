7e637e2bd7894dd6ba981ecc2f680f113efff95f
Yitzchok <adminjew@gmail.com>
2011-11-21 01:52:11 -0500

    Renaming db.i

--- src/Native/swig_and_compile.sh
+++ src/Native/swig_and_compile.sh
@@ -1,6 +1,6 @@
 # Run SWIG
-swig -I./leveldb -v -O -c++ -csharp -namespace LevelDBWrapper.Native -dllimport leveldb_managed db.i
+swig -I./leveldb -v -O -c++ -csharp -namespace LevelDBWrapper.Native -dllimport leveldb_managed leveldb.i
 # Compile c++ file
-gcc -c -fpic -m32 *.cxx -o leveldb_native.o
+gcc -c -fpic -m32 *.cxx -o leveldb_managed.o
 # Compile CSharp files with mono
 gmcs *.cs -out:LevelDBWrapper.dll -target:library
